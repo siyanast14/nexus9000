f217611b16a479ed7d58b8a95681428705b27277
siyanast14 <siyanast@icloud.com>
2023-08-29 20:31:03 +0300

    new commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,16 +1,7 @@
 import axios from "axios";
-var root = 'root';
-var password = 'password';
 export default axios.create({
  baseURL: "http://localhost:8084/user",
  headers: {
  "Content-type": "application/json"
  },
- auth: {
-
-    username: root,
-
-    password: password
-
-  }
-});
+ });
